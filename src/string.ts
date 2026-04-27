df845897d4afcb55493bc19f7bf3ae7249d2a8cd
Yashesh Tamboli <99063337+yasheshcygbit@users.noreply.github.com>
2026-04-28 00:28:09 +0530

    fix(string): strip non-alphanumerics in slugify (#4)
    (cherry picked from commit 000a1129b4bb0537e3ba6dc23d714a13a85ae16e)

--- src/string.ts
+++ src/string.ts
@@ -8,5 +8,5 @@ export function reverse(str: string): string {
 }
 
 export function slugify(str: string): string {
-  return str.toLowerCase().trim().replace(/\s+/g, '-');
+  return str.toLowerCase().trim().replace(/[^a-z0-9\s-]/g, '').replace(/\s+/g, '-');
 }
