09bf3fbf221a6dd454655d0c09498b7bdc022ea9
Yashesh Tamboli <99063337+yasheshcygbit@users.noreply.github.com>
2026-04-28 01:24:44 +0530

    fix(string): lowercase remainder in capitalize (#6)
    (cherry picked from commit 74a388769197295b05a93322ca3cbc826aed176e)

--- src/string.ts
+++ src/string.ts
@@ -1,6 +1,6 @@
 export function capitalize(str: string): string {
   if (!str) return str;
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 export function reverse(str: string): string {
